3ab6701dc70c7425e10d24bd4d90565f53a708bb
Kgad <kalisagad05@gmail.com>
2026-01-03 16:02:04 +0200

    updated team page

--- models/Team.js
+++ models/Team.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true },
   position: { type: String, required: true },
-  category: { type: String, default: 'Uncategorized' },
+  category: { type: [String], default: ['Uncategorized'] },
   image: { type: String, required: true },
   phone: { type: String, default: '' },
   linkedin: { type: String, default: '' },
